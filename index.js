19e135b1042ba122a0b7dfe3176935c5680b247c
saahith23 <saahithvaspari23@gmail.com>
2021-09-25 17:18:24 +0530

    minor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var outputBox = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/pig-latin.json"
 
 function getTranslationURL(text){
-    var url = serverURL + "?"+ "text" = +text
+    var url = serverURL + "?"+ "text=" +text;
     console.log(url);
     return url
 }
