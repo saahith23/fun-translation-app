28ae1c32d7568cb73ab9c0ce093b586aba6356d6
saahith23 <saahithvaspari23@gmail.com>
2021-09-25 17:12:35 +0530

    css

---
+++ index.js
@@ -0,0 +1,30 @@
+var textArea = document.querySelector("#textarea");
+var btnTranslate = document.querySelector("#button");
+var outputBox = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json"
+
+function getTranslationURL(text){
+     var url = serverURL + "?"+ "text" = +text
+     console.log(url);
+     return url
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong try again later");
+}
+
+function clickHandler(){
+
+    var inputText =txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText =json.contents.translated;
+        outputBox.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+btnTranslate.addEventListener('click', clickHandler)
